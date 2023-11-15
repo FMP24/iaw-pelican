9bd97bc5ae1f88eb659659669da12aeab8c79c5c
FMP24 <muperezfrancisco@gmail.com>
2023-11-15 18:57:30 +0100

    Actualización automática del sitio

--- renderpelican.sh
+++ renderpelican.sh
@@ -11,20 +11,18 @@ BRANCH="$1"
 
 # No correr como root
 if [ $(id -u) -eq 0 ]; then
-  echo "¡¡No corras este script con permisos de root!!"
-  exit 1
+        echo "¡¡No corras este script con permisos de root!!"
+        exit 1
 fi
 
 # Error de uso
-if [ "$#" -ne 2 || "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ] && [ "$#" -ne 1 ]; then
         echo "Uso: $0 <Nombre rama> [Origen (Por defecto: origin)] "
         exit 1
 fi
 
 # Origen default -> origin
-if [ "$#" -eq 1 ]; then
-	ORIGIN="origin"
-fi
+[ "$#" -eq 1 ] && ORIGIN="origin"
 
 #Script
 pelican -s "$PELICAN_DIR"/pelicanconf.py
@@ -36,12 +34,12 @@ echo "Actualizando repositorio principal"
 cd "$PELICAN_DIR"
 git add . > /dev/null
 git commit -a -m "Actualización automática del sitio" > /dev/null
-git push origin "$BRANCH" > /dev/null
+git push origin "$BRANCH" > /dev/null 2>&1
 
 echo "Actualizando repositorio de salida"
 cd "$OUTPUT_DIR"
 git add . > /dev/null
 git commit -a -m "Actualización automática del sitio" > /dev/null
-git push "$ORIGIN" "$BRANCH" > /dev/null
+git push "$ORIGIN" "$BRANCH" > /dev/null 2>&1
 
 exit 0
